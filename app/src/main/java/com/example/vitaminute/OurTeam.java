31f59fad2279202c18af7717f21e42c40d136542
unknown <giamouridis22@gmail.com>
2020-06-18 13:32:30 +0300

    first commit

---
+++ app/src/main/java/com/example/vitaminute/OurTeam.java
@@ -0,0 +1,14 @@
+package com.example.vitaminute;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OurTeam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_our_team);
+    }
+}
