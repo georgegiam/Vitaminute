df26e06bdabf5eae8c3755081f7a588ac703c44f
unknown <giamouridis22@gmail.com>
2020-06-19 12:10:02 +0300

    our team added

--- app/src/main/java/com/example/vitaminute/OurTeam.java
+++ app/src/main/java/com/example/vitaminute/OurTeam.java
@@ -3,12 +3,33 @@ package com.example.vitaminute;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OurTeam extends AppCompatActivity {
+
+    private TextView team1, team2, team3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_our_team);
+
+        team1 = (TextView) findViewById(R.id.team1_textView);
+        team2 = (TextView) findViewById(R.id.team2_textView);
+        team3 = (TextView) findViewById(R.id.team3_textView);
+
+        team1.setText("Elizabeth opens the line of communication between clients, customers, " +
+                        "and businesses to get projects done. With over 20 years in both public and " +
+                        "private sectors, Elizabeth has experience in management consultation, team building," +
+                        " professional development, strategic implementation, and company collaboration.");
+        team2.setText("Alexis connects online brands to their target audiences for the " +
+                " marketing experience. At the University of California, Alexis, learned the " +
+                "importance of applying classic marketing techniques to modern brand strategies from " +
+                "experts in the field, including her Communications professor, Dr. Martin Mitchell.");
+        team3.setText("Jack knows that successful students become successful adults. This is " +
+                "his 15th year at Edison Elementary School and her 10th year teaching fourth grade. " +
+                "So far, fourth grade is his favorite grade to teach! Mrs. Carroll was the 2011 Newell " +
+                "Unified School District Teacher of the Year, and received his National Board Certification " +
+                "in 2013.");
     }
 }
--- app/src/main/java/com/example/vitaminute/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/vitaminute/ui/home/HomeViewModel.java
@@ -10,7 +10,11 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Founded in 2020 by George Giamouridis, tech freaks has come a long way from its " +
+                "beginnings in Greece. When George Giamouridis first started out, his passion for" +
+                "nutrition and physique drove him to do tons of research so that tech freaks can offer " +
+                "you a completely nutrition information platform. We now serve customers all over the" +
+                "world, and are thrilled that we're able to turn our passion into our own website.");
     }
 
     public LiveData<String> getText() {
