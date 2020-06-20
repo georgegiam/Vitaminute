9df51aad8cd985405f766a0fab67d247e68c0410
unknown <giamouridis22@gmail.com>
2020-06-20 17:25:38 +0300

    gallery added

---
+++ app/src/main/java/com/example/vitaminute/FullScreenActivity.java
@@ -0,0 +1,25 @@
+package com.example.vitaminute;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class FullScreenActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen);
+
+        Intent i = getIntent();
+
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        imageView.setImageResource(imageAdapter.imageArray[position]);
+    }
+}
--- app/src/main/java/com/example/vitaminute/Gallery.java
+++ app/src/main/java/com/example/vitaminute/Gallery.java
@@ -2,13 +2,31 @@ package com.example.vitaminute;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 public class Gallery extends AppCompatActivity {
+
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        gridView = (GridView) findViewById(R.id.grid_view);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), FullScreenActivity.class);
+                intent.putExtra("id", position);
+                startActivity(intent);
+            }
+        });
     }
 }
